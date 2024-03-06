fbbd8b36847dc6cb56abfe1cb6c1107c7c582f5e
Ritesh Solanki <ritesh.solanki6@gmail.com>
2024-03-06 18:22:28 +0000

    Change port

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 const express = require("express"); 
 const cors = require("cors");
 const app = express(); 
-const PORT = process.env.PORT || 8083; 
+const PORT = process.env.PORT || 4000; 
 
 var corsOptions = {
     // url1: "http://localhost:3000",
